a69b4ad8654d265f47a3a7177259e575a46ee20f
allan <allanmathenge2@gmail.com>
2024-01-16 00:47:18 +0300

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const Stripe = require("stripe");
 
 const app = express();
 app.use(cors());
@@ -106,6 +107,48 @@ app.get("/product", async (req, res) => {
   const data = await productModel.find({});
 
   res.send(JSON.stringify(data));
+});
+
+/* Payment gateway */
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/checkout-payment", async (req, res) => {
+  try {
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1OYbSVFkEFJH7MWThqjUJMmm" }],
+
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "KES",
+            product_data: {
+              name: item.name,
+            },
+            unit_amount: item.price * 100,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+          },
+          quantity: item.qty,
+        };
+      }),
+
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+    };
+
+    const session = await stripe.checkout.sessions.create(params);
+
+    res.status(200).json(session.id);
+  } catch (error) {
+    res.status(error.statusCode || 500).json(error.message);
+  }
 });
 
 app.listen(PORT, () => console.log("Server is running at port : " + PORT));
